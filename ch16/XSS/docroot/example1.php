cc5689008f6a7d319c51869e07a6bd143f452103
root <sussurro@happypacket.net>
2017-09-30 02:20:58 -0400

    fixed docker setup

--- ch16/XSS/docroot/example1.php
+++
@@ -1,9 +0,0 @@
-<HTML><BODY>
-<PRE>
-You put in the data:
-<?php print($_REQUEST["name"] . "\n")?>
-<?php print($_REQUEST["address"] . "\n")?>
-<?php print($_REQUEST["country"] . "\n")?>
-</PRE>
-</BODY>
-</HTML>
